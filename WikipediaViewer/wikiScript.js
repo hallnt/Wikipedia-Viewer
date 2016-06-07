70e1af13843b9d8d1acda7f12ab4037271e46a9a
Teleisha Hall <nickyb63@hotmail.com>
2016-06-06 21:22:22 -0400

    displaying search results

--- WikipediaViewer/wikiScript.js
+++ WikipediaViewer/wikiScript.js
@@ -1,28 +1,33 @@
-﻿$(document).ready(function () {
-    // Submit button click function to search for a term 
+﻿$(document).ready(function() {
+
+    /*******************************************************
+        Search button click function to search for a term
+     *******************************************************/
     $("#search").click(function () {
+        // clear results section
+        $("#searchResults").html("");  
+      
         // Get input from input field
         var searchTerm = $("#searchTerm").val();
-        //var url = "https://en.wikipedia.org/wiki/Special:ApiSandbox#action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=jsonfm";
 
         // URL for Wikipedia's web service API
         var wikiApiUrl = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
 
-        // AJAX JQuery to return search results
+        // Use AJAX to return search results
         $.ajax({
             type: "GET",
             dataType: "json",
             url: wikiApiUrl,
             async: false,
-            success: function (data) {
-                console.log(data[1][0]);
-                console.log(data[2][0]);
-                console.log(data[3][0]);
+            success: function (data) {              
+                for (var i=0; i<data[1].length; i++) {
+                    $("#searchResults").append("<li><strong><em>" + data[1][i] + "</em></strong></br>" + data[2][i] + "</br>" + data[3][i] + "</a></li>");
+                }                
             },
             error: function (err) {
                 alert("error");
             }
-        }); // end of ajax function   
+        }); // end of ajax  
 
     }); // end of click function  
 
